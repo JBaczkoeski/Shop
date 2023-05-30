c004ecc772e2e8c6314285d3878de97c36b5d9cc
JBaczkowski <baczkowskikuba2@gmail.com>
2023-05-30 18:10:39 +0200

    Start project

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Shop</title>
+
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light mt-2 mx-4">
+    <div class="container-fluid">
+        <a class="navbar-brand">Shop</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Strona główna</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Produkty</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active">Wyprzedaż</a>
+                </li>
+            </ul>
+            <form class="container col-8 row">
+                <div class="container col-8 me-0">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                </div>
+                <div class="container col-4 ms-0">
+                    <button class="btn btn-outline-success" type="submit">Szukaj</button>
+                </div>
+            </form>
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link active" href="{{ route('login') }}">Logowanie</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link active" href="{{ route('register') }}">Rejestracja</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                Wyloguj
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container-fluid">
+    @yield('content')
+</div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.navbar')
+
+@section('content')
+    <div class="row justify-content-center p-1">
+        <img src="{{ url('storage/Images/baner.png') }}" alt="Computer image" class="rounded-5">
+        <div class="container col-12 py-2">
+            <hr>
+            <h1 class="text-center">Polecamy</h1>
+            <hr>
+            <div class="row justify-content-center">
+                @for($i = 1; $i <= 6; $i++)
+                    <div class="card mx-5 my-3 col-12 col-lg-3 border-0 text-center" style="background-color: #f8f9fa">
+                        <img class="card-img-top rounded-3 mt-3" src="{{ url('storage/Product/mac-book.jpg') }}"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Mac Book</h5>
+                            <div class="container col-12 mb-4">
+                                <div class="row">
+                                    <div class="col-6">Producent</div>
+                                    <div class="col-6">Apple</div>
+                                    <div class="col-6">Rok produkcji</div>
+                                    <div class="col-6">966</div>
+                                    <div class="col-6">Nr seryjny</div>
+                                    <div class="col-6">AH5252</div>
+                                </div>
+                            </div>
+                            <a href="#" class="btn btn-success">Sprawdź</a>
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        </div>
+        <div class="container col-12 text-center">
+            <hr>
+            <h1>Wybrane promocje</h1>
+            <hr>
+            <div class="row justify-content-center">
+                @for($i = 1; $i <= 4; $i++)
+                    <div class="card mx-5 my-3 col-12 col-lg-2 border-0 text-center" style="background-color: #f8f9fa">
+                        <img class="card-img-top rounded-3 mt-3" src="{{ url('storage/Product/mac-book.jpg') }}"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Mac Book</h5>
+                            <div class="container col-12 mb-4">
+                                <div class="row">
+                                    <div class="col-6">Producent</div>
+                                    <div class="col-6">Apple</div>
+                                    <div class="col-6">Rok produkcji</div>
+                                    <div class="col-6">966</div>
+                                    <div class="col-6">Nr seryjny</div>
+                                    <div class="col-6">AH5252</div>
+                                </div>
+                            </div>
+                            <a href="#" class="btn btn-success">Sprawdź</a>
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        </div>
+        <div class="container col-12 justify-content-center text-center">
+            <div class="container col-7 my-4 rounded-3 shadow p-5 ps-5">
+                <h1>Newstseler</h1>
+                <p>Nie przegap żadnej promocji,</p>
+                <p>zdobywaj dodatkowe rabaty.</p>
+                <form method="post" action="" class="col-12 row">
+                    <div class="container col-8 ms-5">
+                        <input type="email" placeholder="Wpisz swój e-mail" class="form-control">
+                    </div>
+                    <div class="container col-3">
+                        <button type="submit" class="btn btn-success">Zapisz</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
